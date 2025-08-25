1a4923600281eecdba852e1565b232562888376d
Ellen <ellen.en62@gmail.com>
2025-08-25 10:41:54 -0300

    "Atualizando jogo: adicionando escolha de dificulade e validação de entrada"

--- src/JogoAdivinhcao.java
+++ src/JogoAdivinhcao.java
@@ -4,16 +4,17 @@ public class JogoAdivinhcao {
     private int numeroSecreto;
     private int tentativas;
 
-    public JogoAdivinhcao() {
+    public JogoAdivinhcao(int maxTentativas) {
         Random random= new Random();
         this.numeroSecreto = random.nextInt(100) + 1;
-        this.tentativas = 5;
+        this.tentativas = maxTentativas;
     }
 
     public boolean jogar(int palpite){
         tentativas --;
 
         if (palpite == numeroSecreto){
+            System.out.println("-");
             System.out.println("__>>>>>>>>>><<<<<<<<<<__");
             System.out.println("Parabéns, você acertou!");
             return true;
@@ -24,6 +25,7 @@ public class JogoAdivinhcao {
         }
 
         if (tentativas == 0){
+            System.out.println("-");
             System.out.println("__>>>>>>>>>><<<<<<<<<<__");
             System.out.println("GAME OVER! O número era " + numeroSecreto);
             return true;
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,59 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        JogoAdivinhcao jogo = new JogoAdivinhcao();
+        int nivel = 0;
+        int maxTentativas = 5;
+
+    while (true) {
+        try {
+
+            System.out.println("-");
+            System.out.println("Escolha a dificuldade: ");
+            System.out.println("1- Fácil (10 tentativas)");
+            System.out.println("2- Médio (5 tentativas)");
+            System.out.println("3- Difícil (3 tentativas)");
+
+            nivel = sc.nextInt();
+
+            maxTentativas = switch (nivel) {
+                case 1 -> 10;
+                case 2 -> 5;
+                case 3 -> 3;
+                default -> 5;
+            };
+
+            break;
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida! Digite apenas números de 1 a 100.");
+            System.out.println("x.x");
+            sc.next();
+        }
+    }
+
+        JogoAdivinhcao jogo = new JogoAdivinhcao(maxTentativas);
 
         boolean fim = false;
         while (!fim){
-            System.out.println("-------------------------");
-            System.out.println("Digite seu palpite (1 - 100): ");
-            int palpite = sc.nextInt();
-            fim = jogo.jogar(palpite);
+          try {
+              System.out.println("-------------------------");
+              System.out.println("Digite seu palpite (1 - 100): ");
+              int palpite = sc.nextInt();
+
+              if (palpite < 1 || palpite > 100) {
+                  System.out.println("Por favor, digite um numero entre 1 e 100!");
+                  continue;
+              }
+
+              fim = jogo.jogar(palpite);
+
+          } catch (InputMismatchException e){
+              System.out.println("Entrada inválida! Digite apenas números");
+              System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+              sc.next();
+          }
         }
         sc.close();
     }
